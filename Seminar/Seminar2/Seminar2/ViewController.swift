289ef01a7733f99cef9ba1fae1a68622bb1a2a29
daminoworld <ekadls757@gmail.com>
2022-10-12 17:34:17 +0900

    [ADD] Seminar, Assignment 폴더 및 프로젝트 추가

---
+++ Seminar/Seminar2/Seminar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar2
+//
+//  Created by 김담인 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
